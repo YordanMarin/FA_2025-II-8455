49676c2af3185bb2a25803f4bcb2624e3c234ce6
yordan <yordan@gmail.com>
2025-09-05 08:19:40 -0500

    subiendo ejer1 de la semana1

---
+++ SEMANA1-C#/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+
+        static void ejer1()
+        {
+            string nombre, carrera;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, bienvenido al FA de {carrera}");
+        }
+
+    }
+}
---
+++ SEMANA1_PY/SEMANA1_PY.py
@@ -0,0 +1,7 @@
+def ejer1():
+    nombre = input("Ingrese su nombreP: ")
+    carrera = input("Ingrese su carrera: ")
+
+    print(f"\n{nombre}, bienvenido al FA de {carrera}")
+
+ejer1()
